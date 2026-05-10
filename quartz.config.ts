327f52bdf75511c912c21b3135a327e7919494a4
Moni <mjgrzenda@gmail.com>
2026-05-10 17:44:29 -0400

    display capitalization

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Navria Lore",
+    pageTitle: "Navria Codex",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,7 +35,16 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      mapFn: (node) => {
+        if (node.displayName === "lore") node.displayName = "Lore"
+        if (node.displayName === "regions") node.displayName = "Regions"
+        if (node.displayName === "factions") node.displayName = "Factions"
+        if (node.displayName === "figures") node.displayName = "Figures"
+
+        return node
+      },
+    }),
   ],
   right: [
     //Component.Graph(),
@@ -59,7 +68,16 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      mapFn: (node) => {
+        if (node.displayName === "lore") node.displayName = "Lore"
+        if (node.displayName === "regions") node.displayName = "Regions"
+        if (node.displayName === "factions") node.displayName = "Factions"
+        if (node.displayName === "figures") node.displayName = "Figures"
+
+        return node
+      },
+    }),
   ],
   right: [],
 }
